cb550419921baf984cea86b24177bef5e0ad6355
Abhi <abhiks177@gmail.com>
2020-12-09 22:03:41 +0530

    0.0.5

--- recme.py
+++ recme.py
@@ -1,8 +1,19 @@
 import numpy as np
-from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
+from lightfm.datasets import fetch_movielens
 
 data = fetch_movielens()
 
 print(repr(data['train']))
 print(repr(data['test']))
+
+model = LightFM(loss="warp")
+model.fit(data['train'], epochs=50, num_threads=3)
+
+
+def rec_init(model, data, user_id):
+    n_users, n_items = data['train'].shape
+    for user in user_id:
+        known_positive = data['item_labels'][data['train'].tocsr()[user].indices]
+        scores = model.predict(user, np.arrange(n_items))
+        top_items = data['item_labels'][np.argsort(scores)]

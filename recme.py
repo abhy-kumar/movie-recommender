73dcd0849f3b5cdc3e0b99357f7f5a5353d8c7e5
Abhi <abhiks177@gmail.com>
2020-12-10 23:28:34 +0530

    refactoring

--- recme.py
+++ recme.py
@@ -4,16 +4,16 @@ from lightfm.datasets import fetch_movielens
 
 data = fetch_movielens()
 
-print(repr(data['train']))
-print(repr(data['test']))
+print(repr(data["train"]))
+print(repr(data["test"]))
 
 model = LightFM(loss="warp")
-model.fit(data['train'], epochs=50, num_threads=3)
+model.fit(data["train"], epochs=50, num_threads=3)
 
 
 def rec_init(model, data, user_id):
-    n_users, n_items = data['train'].shape
+    n_users, n_items = data["train"].shape
     for user in user_id:
-        known_positive = data['item_labels'][data['train'].tocsr()[user].indices]
+        known_positive = data["item_labels"][data["train"].tocsr()[user].indices]
         scores = model.predict(user, np.arrange(n_items))
-        top_items = data['item_labels'][np.argsort(scores)]
+        top_items = data["item_labels"][np.argsort(scores)]
